655ebd75d1bbe336ed605668ccf3d22b7c6f7dd9
vercel97 <veronichaceline5@gmail.com>
2023-10-01 15:14:23 +0200

    Code for tutorial 3 weekly assignments 5 DAT250

---
+++ src/test/java/com/example/tutorial3/Tutorial3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorial3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
